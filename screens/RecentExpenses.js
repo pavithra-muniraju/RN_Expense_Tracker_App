3d58220336e9b2f49a0deea310b3217df47c9d42
Pavithra Muniraju <pavithra.muniraju@ust.com>
2026-03-05 16:14:06 +0530

    first push with hello

---
+++ screens/RecentExpenses.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function RecentExpenses() {
+    return(
+        <View>
+            <Text>RecentExpenses</Text>
+        </View>
+    )
+}
+
+export default RecentExpenses;
